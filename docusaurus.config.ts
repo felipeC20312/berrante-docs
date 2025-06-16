5a783bf8a11fcaeef6e462197cc6168ac2c7bb53
felipeC20312 <felipecaetanodev@gmail.com>
2025-06-16 14:03:04 -0300

    Setup - First

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import { SITE_URL } from './envconfig';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -15,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: SITE_URL,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/berrante-docs',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
---
+++ envconfig.ts
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const DOCUSAURUS_ENV = process.env.DOCUSAURUS_ENV;
+export const SITE_URL = process.env.SITE_URL;
